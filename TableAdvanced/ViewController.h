7620fea5cf70e0b4b119844d2af7cba9dac42209
hyper1FU <yifu.liao@gmail.com>
2014-03-07 20:19:10 +0900

    first commit

--- TableAdvanced/ViewController.h
+++ TableAdvanced/ViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+<UITableViewDataSource,UITableViewDelegate>
+{
+    IBOutlet UITableView *table;
+    NSMutableArray *nameArray;//名前
+    NSMutableArray *jobArray;//職業名の配列
+    NSMutableArray *profileArray;//プロフィール
+    NSMutableArray *imageArray;//写真用の配列
+}
 
 @end
--- TableAdvanced/ViewController.m
+++ TableAdvanced/ViewController.m
@@ -17,6 +17,57 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    table.delegate  =   self;
+    table.dataSource    =   self;
+    nameArray       =   [[NSMutableArray alloc]initWithObjects:
+                     @"哀川翔",
+                     @"イチロー",
+                     @"宇多田ヒカル",
+                     @"エジソン",
+                     @"おぎやはぎ",
+                     @"カトパン",
+                     @"桐谷美玲",
+                     @"くまモン",
+                     @"件どー小林",
+                     @"小森勇太",
+                     nil];
+    jobArray        =   [[NSMutableArray alloc]initWithObjects:
+                     @"俳優",
+                     @"メジャーリーガー",
+                     @"歌手",
+                     @"発明家",
+                     @"お笑いコンビ"
+                     @"アナウンサー",
+                     @"ファッションモデル",
+                     @"ご当地キャラクター",
+                     @"お笑い芸人",
+                     @"謎解きマスター",
+                     nil];
+    profileArray    =   [[NSMutableArray alloc]initWithObjects:
+                         @"1",
+                         @"2",
+                         @"3",
+                         @"4",
+                         @"5",
+                         @"6",
+                         @"7",
+                         @"8",
+                         @"9",
+                         @"A",
+                         nil];
+    imageArray      =   [[NSMutableArray alloc]initWithObjects:
+                         [UIImage imageNamed:@"aikawa_sho.jpg"],
+                         [UIImage imageNamed:@"ichiro.jpg"],
+                         [UIImage imageNamed:@"utada_hikaru.jpg"],
+                         [UIImage imageNamed:@"edison.jpg"],
+                         [UIImage imageNamed:@"ogiyahagi.jpg"],
+                         [UIImage imageNamed:@"katopan.png"],
+                         [UIImage imageNamed:@"kiritani_mirei.jpg"],
+                         [UIImage imageNamed:@"kumamon.jpg"],
+                         [UIImage imageNamed:@"kendo_kobayashi.jpg"],
+                         [UIImage imageNamed:@"komori_yuta.jpg"],
+                         nil];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
